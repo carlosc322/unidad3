7824cfd8732847d5bc9a923e4b45ebd7d2b3a30d
carlosc322 <carlchoque52@gmail.com>
2025-07-03 17:59:59 -1200

    Segundo Commit
    El commit contiene código del formulario para probar los inputs, también se creo la función de agregarDatos, se estableció el hook useEffect  con el objetivo de mostrar los dato ingresados, en la consola del navegador y recuperar el valor de los inputs.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,62 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from "react"
+
+const Datos = {// nombreG-Datos nombreG-name tienen que ser iguales//
+  nombreG: "",
+  idG: "",
+  fechaG: "",
+  descripcionG: "",
+  turnoG: ""
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  console.log("esto es home")
+  const[lista, setLista] = useState(Datos)
+  console.log(lista ,"se imprime en vivo")
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+
+  useEffect(()=>{
+    const localS = window.localStorage
+    localS.setItem("datos",JSON.stringify(lista))
+  })
+  
+
+  const agregarDatos = (name:string,value:string)=>{ //TypeScript//
+    console.log("esto es agregarD")
+    console.log(name,value)
+    setLista({...lista, [name] : value})
+  };
+
+
+
+
+    return (
+    <>
+      <h1>Control Laboral</h1>
+
+      <h1>{lista.nombreG}</h1>
+    <form>
+      <label>Grupo de Trabajo : </label>
+      <input type="text" name="nombreG" placeholder="Ingresa el nombre.." 
+      onChange = {(e) => {agregarDatos(e.target.name, e.target.value)}}
+      /><br />
+
+      <label>ID del Grupo : </label>
+      <input type="number" name="id" placeholder="Ingresa el id.."/><br />
+      <label>Fecha : </label>
+      <input type="date" name="fecha" placeholder="Ingrese fecha de laburo.." /><br />
+      <label>Descripción. </label><br />
+      <textarea name="Descripcion" placeholder="Descripcion del trabajo.."></textarea><br />
+      <label>Turno : </label>
+      <select>
+        <option value="dia">Dia</option>
+        <option value="noche">Noche</option>
+      </select>
+
+    </form>
+    </>
+
+  )
 }
